ba279ba03f02ef13e13e7c60bf2c6fb2ae5bce32
Ricardo Casanova <ricardo.casanova@outlook.com>
2024-06-12 13:32:21 +0200

    Set minimum iOS version to 15.0 and add APIClient tests

---
+++ AstroPics/AstroPicsTests/Modules/APIClientTests.swift
@@ -0,0 +1,37 @@
+import XCTest
+
+@testable import AstroPics
+
+final class APIClientTests: XCTestCase {
+  func testAPIClientGetAstronomyPicturesSuccess() async {
+    // Given
+    let apiClient = APIClient.mock
+
+    // When
+    let result = await apiClient.getAstronomyPicturesURL(startDate: "2024-06-01", endDate: "2024-06-07")
+
+    // Then
+    switch result {
+    case .success(let pictures):
+      XCTAssertFalse(pictures.isEmpty, "Gyms list should not be empty")
+    case .failure(let error):
+      XCTFail("Expected success, but got failure with error: \(error)")
+    }
+  }
+
+  func testAPIClientGetAstronomyPicturesFailure() async {
+    // Given
+    let apiClient = APIClient.failureMock
+
+    // When
+    let result = await apiClient.getAstronomyPicturesURL(startDate: "2024-06-01", endDate: "2024-06-07")
+
+    // Then
+    switch result {
+    case .success:
+      XCTFail("Expected failure, but got success")
+    case .failure(let error):
+      XCTAssertEqual(error, .serverError(code: 999), "Expected server error with specific code")
+    }
+  }
+}
