b579cd64e619e8bbfe7e7f11f22d89faf5e75565
Ricardo Casanova <ricardo.casanova@outlook.com>
2024-06-12 12:56:35 +0200

    Add initial Xcode project

---
+++ AstroPics/AstroPics/AstroPicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroPicsApp.swift
+//  AstroPics
+//
+//  Created by Ricardo Casanova on 12/6/24.
+//
+
+import SwiftUI
+
+@main
+struct AstroPicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
