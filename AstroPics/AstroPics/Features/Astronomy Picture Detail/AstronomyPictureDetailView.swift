f11b955b13ba63b67666cc4856ab01ddc0ab32d9
Ricardo Casanova <ricardo.casanova@docusketch.com>
2025-03-09 22:08:06 +0100

    Update hasVideoContent property by contentType

--- AstroPics/AstroPics/Features/Astronomy Picture Detail/AstronomyPictureDetailView.swift
+++ AstroPics/AstroPics/Features/Astronomy Picture Detail/AstronomyPictureDetailView.swift
@@ -22,7 +22,7 @@ struct AstronomyPictureDetailView: View {
   @ViewBuilder
   private var content: some View {
     ZStack {
-      if store.astronomyPicture.hasVideoContent {
+      if store.astronomyPicture.contentType == .video {
         Color.black
           .frame(maxWidth: .infinity, maxHeight: .infinity)
       } else {
@@ -65,7 +65,7 @@ struct AstronomyPictureDetailView: View {
       VStack(alignment: .leading, spacing: 8) {
         Spacer()
 
-        if store.astronomyPicture.hasVideoContent {
+        if store.astronomyPicture.contentType == .video {
           VStack {
             Spacer()
             HStack {
@@ -101,7 +101,7 @@ struct AstronomyPictureDetailView: View {
     .background(.black)
     .toolbar {
       ToolbarItem(placement: .navigationBarTrailing) {
-        if !store.astronomyPicture.hasVideoContent {
+        if store.astronomyPicture.contentType == .image {
           Button(action: {
             store.send(.didTapOnLoadContent)
           }) {
@@ -164,7 +164,7 @@ struct AstronomyPictureDetailView_Preview {
         date: "2024-01-01",
         title: "NGC 1232: A Grand Design Spiral Galaxy",
         url: URL(string: "https://apod.nasa.gov/apod/image/2401/ngc1232b_vlt_960.jpg")!,
-        hasVideoContent: false,
+        contentType: .image,
         explanation:
           "Can a rocket make the Moon ripple? No, but it can make a background moon appear wavy.  The rocket, in this case, was a SpaceX Falcon Heavy that blasted off from NASA's Kennedy Space Center last week. In the featured launch picture, the rocket's exhaust plume glows beyond its projection onto the distant, rising, and nearly full moon.  Oddly, the Moon's lower edge shows unusual drip-like ripples. The Moon itself, far in the distance, was really unchanged.  The physical cause of these apparent ripples was pockets of relatively hot or rarefied air deflecting moonlight less strongly than pockets of relatively cool or compressed air: refraction. Although the shot was planned, the timing of the launch had to be just right for the rocket to be transiting the Moon during this single exposure."
       )
@@ -184,7 +184,7 @@ struct AstronomyPictureDetailView_Preview {
         date: "2024-01-01",
         title: "NGC 1232: A Grand Design Spiral Galaxy",
         url: URL(string: "https://www.youtube.com/embed/PpyPgJHKxSw?rel=0")!,
-        hasVideoContent: true,
+        contentType: .video,
         explanation:
           "Can a rocket make the Moon ripple? No, but it can make a background moon appear wavy.  The rocket, in this case, was a SpaceX Falcon Heavy that blasted off from NASA's Kennedy Space Center last week. In the featured launch picture, the rocket's exhaust plume glows beyond its projection onto the distant, rising, and nearly full moon.  Oddly, the Moon's lower edge shows unusual drip-like ripples. The Moon itself, far in the distance, was really unchanged.  The physical cause of these apparent ripples was pockets of relatively hot or rarefied air deflecting moonlight less strongly than pockets of relatively cool or compressed air: refraction. Although the shot was planned, the timing of the launch had to be just right for the rocket to be transiting the Moon during this single exposure."
       )
